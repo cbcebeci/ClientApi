2ace2fbe8fe5c89ed1dfd6bb8f057e26642fd267
Can Berk Cebeci <canberk_cebeci@hotmail.com>
2020-09-06 17:37:43 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/service/ClientService.java
@@ -0,0 +1,42 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.ClientDao;
+import com.example.demo.model.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ClientService {
+
+    private final ClientDao clientDao;
+
+    @Autowired
+    public ClientService(@Qualifier("fakeDao") ClientDao clientDao) {
+        this.clientDao = clientDao;
+    }
+
+    public int addClient(Client client){
+        return clientDao.insertClient(client);
+    }
+
+    public List<Client> getAllClient(){
+        return clientDao.selectAllClient();
+    }
+
+    public Optional<Client> getClientById(UUID id){
+        return clientDao.selectClientById(id);
+    }
+
+    public int deleteClient(UUID id){
+        return clientDao.deleteClientById(id);
+    }
+
+    public int updateClient(UUID id, Client newClient){
+        return clientDao.updateClientById(id, newClient);
+    }
+}
